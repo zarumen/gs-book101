5830fd6f3e18a92d4e83e6c105a78ec326f2b3b1
zarumen <prinzbiz@gmail.com>
2020-03-28 16:38:37 +0700

    add gitignore and dependencies

---
+++ server.js
@@ -0,0 +1,18 @@
+//ทำการ import express เข้ามาใช้งาน โดยสร้างตัวแปร express ขึ้นมาเพื่อรับค่า
+const express = require('express')
+//ทำการสร้าง Instance ของ express และสร้างตัวแปร app ขึ้นมาเพื่อรับค่า
+const app = express()
+//สร้างตัวแปร PORT ขึ้นมารับค่า port ในกรณีที่เราได้กำหนดไว้ใน environment ของเครื่อง
+//แต่ถ้าไม่ได้กำหนดไว้ เราจะใช้ค่า 8000 แทน
+const PORT = process.env.PORT || 8000
+//สร้าง route ขึ้นมา 1 ตัว โดยกำหนดให้ path คือ / หรือ index ของ host นั่นเอง
+//จากนั้นให้กำหนด response แสดงคำว่า Hello World
+app.get('/', (req, res) => res.send('Hello World'))
+//run web server ที่เราสร้างไว้ โดยใช้ PORT ที่เรากำหนดไว้ในตัวแปร PORT
+app.listen(PORT, () => {
+    //หากทำการ run server สำเร็จ ให้แสดงข้อความนี้ใน cmd หรือ terminal
+    console.log(`Server is running on port : ${PORT}`)
+})
+//ทำการ export app ที่เราสร้างขึ้น เพื่อให้สามารถนำไปใช้งานใน project อื่นๆ 
+//เปรียบเสมือนเป็น module ตัวนึง
+module.exports = app
